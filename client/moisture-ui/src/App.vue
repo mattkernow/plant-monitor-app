890d87b00b129dd7d11cc9d7d92cd17226c4ebb6
mattkernow <walsh.gis@gmail.com>
2020-07-16 21:10:57 +0100

    Create first component

--- client/moisture-ui/src/App.vue
+++ client/moisture-ui/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SensorDisplay/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SensorDisplay from './components/SensorDisplay.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SensorDisplay
   }
 }
 </script>
---
+++ client/moisture-ui/src/components/SensorDisplay.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="sensorDisplay">
+        Hello World
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SensorDisplay'
+}
+</script>
+
+<style scoped></style>
